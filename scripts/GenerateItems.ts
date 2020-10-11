a7079886f04a4e94c766736c10ccd9a45ef2d14c
evenstephenr <github@ramirs.com>
2020-10-11 18:23:26 -0400

    (scripts:GenerateItems):init scripts module, add script for generating a bunch of items for analysis

---
+++ scripts/GenerateItems.ts
@@ -0,0 +1,17 @@
+import { Treasure, Item } from "../Models/Treasure.ts";
+
+/** (example) $ deno run scripts/GenerateItems.ts 10 */
+const itemCount = Deno.args[0] || 100;
+/** (example) $ deno run --allow-write scripts/GenerateItems.ts 100 ./scripts/sample.json */
+const persistence = Deno.args[1] || undefined;
+
+const items: Item[] = [];
+for (let i = 0; i < itemCount; i++) {
+  items.push(new Treasure().getStats());
+}
+
+if (persistence) {
+  Deno.writeTextFile(persistence, JSON.stringify(items, null, 2));
+} else {
+  console.log(items);
+}
