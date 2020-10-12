f4e396a4dbc8e84f70d84b9470a55affa40d8b4f
evenstephenr <github@ramirs.com>
2020-10-12 11:12:43 -0400

    2(Treasure):simplify logic to calculate item stats

--- Models/Treasure.ts
+++ Models/Treasure.ts
@@ -1,5 +1,5 @@
 import { Surprise } from "./Surprise.ts";
-import { generateRandomNumber, coinFlip } from "../util/Random.ts";
+import { coinFlip, generateRandomNumber } from "../util/Random.ts";
 
 export type Item = {
   weight: number;
@@ -25,25 +25,12 @@ export class Treasure implements Surprise {
   generateItem() {
     let weight = 1;
     const itemStats = ["health", "strength", "speed"].reduce((acc, item) => {
-      const skip = coinFlip();
-      if (skip) {
-        return acc;
-      }
-
-      const isGood = coinFlip();
-      if (isGood) {
-        const impact = generateRandomNumber(1, 3);
-        if (impact > 1 && 3 >= impact) weight += 1;
-        if (impact > 3) weight += 1;
-        return {
-          ...acc,
-          [item]: impact,
-        };
-      }
-
+      const impact = generateRandomNumber(0, 8) - 2;
+      if (impact > 1 && 3 >= impact) weight += 1;
+      if (impact > 3) weight += 1;
       return {
         ...acc,
-        [item]: generateRandomNumber(1, 2) * -1,
+        [item]: impact,
       };
     }, {});
 
@@ -58,10 +45,10 @@ export class Treasure implements Surprise {
 Hmm do you want to pick this up?
 
 ${
-  !this.stats.health && !this.stats.strength && !this.stats.speed
-    ? "...I think it's a rock..."
-    : "(Impact to your gear is shown)"
-}
+      !this.stats.health && !this.stats.strength && !this.stats.speed
+        ? "...I think it's a rock..."
+        : "(Impact to your gear is shown)"
+    }
 
 ${JSON.stringify(this.getStats(), null, 2)}
 
